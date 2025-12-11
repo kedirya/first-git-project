2f887ffaef41e7fc84a5dc67550854a5a00a24c6
Kedirya <kediryaca@gmail.com>
2025-12-11 14:06:50 -0800

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+       
+        
 
     }
 }
